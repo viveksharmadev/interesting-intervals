3b0e552932f17a521ec16de36c8a75f41ccf1cfd
Learner <vivek.gurukul1982@gmail.com>
2020-06-06 11:26:45 -0700

    Update queue-reconstruction-by-height.java

--- queue-reconstruction-by-height.java
+++ queue-reconstruction-by-height.java
@@ -1,4 +1,10 @@
 // https://leetcode.com/problems/queue-reconstruction-by-height/
+/* 1. Pick out tallest group of people and sort them in a subarray (S). Since there's no other groups of people taller than them, therefore each guy's index will be just as same as his k value.
+2. For 2nd tallest group (and the rest), insert each one of them into (S) by k value. So on and so forth.
+E.g.
+input: [[7,0], [4,4], [7,1], [5,0], [6,1], [5,2]]
+subarray after step 1: [[7,0], [7,1]]
+subarray after step 2: [[7,0], [6,1], [7,1]]*/
 class queue-reconstruction-by-height {
     // tc -> n^2 + nlogn -> n^2, sc-> n
     public int[][] reconstructQueue(int[][] people) {
